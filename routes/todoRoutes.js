45478df760c3dd05c2624196fcccedb385905454
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-01-08 11:49:41 -0700

    added post and fixed the id bug in post route

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -5,8 +5,35 @@ const Todos = require("../models/todoModel")
 
 //Get 
 router.get("/todos", (req, res) => {
-    console.log("We got to our get route")
-    res.end()
+    Todos.find((err, todos) => {
+        if(err){
+            res.status(404).json({ message: "Get todos err", error: `${err}`})
+        } else {
+            res.status(200).json([
+                ...todos.map((todo) => {
+                    const {_id, title, done} = todo
+
+                    return{ id: _id, title, done}
+                })
+            ])
+        }
+    })
+})
+
+//post 
+router.post("/todo", (req, res) => {
+    const todo = new Todos(req.body)
+
+    todo
+        .save()
+        .then(todo => {
+            const { _id, title, done} = todo
+    
+            res.status(200).json({id: _id, title, done})
+        })
+        .catch(err => {
+            res.status(400).json({ message: "unable to post", errors: `${err}`})
+        })
 })
 
 module.exports = router 
